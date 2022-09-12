fcfcd0fb00b9ed6c94953394f5a58a000e1cbf85
vanceb <vance@axxe.co.uk>
2022-09-12 15:55:33 +0100

    Using new wifimanager library

--- etask_wifi.h
+++ etask_wifi.h
@@ -5,61 +5,21 @@
  * Platformio Libraries needed
 
 lib_deps = 
-	jchristensen/Timezone@^1.2.4
-	paulstoffregen/Time@^1.6
-	https://github.com/tzapu/WiFiManager.git
-	arduino-libraries/NTPClient@^3.1.0
-	yiannisbourkelis/Uptime Library @ ^1.0.0
+    khoih-prog/ESP_WifiManager @ ^1.11.0
+;    khoih-prog/ESPAsync_WiFiManager @ ^1.14.0
+    khoih-prog/ESP_DoubleResetDetector @ ^1.3.2
 
-*/
-
-#include <Timezone.h>
-
-#define NTP_SERVER "pool.ntp.org"
-#define NTP_UPDATE_INTERVAL 15  // minutes
-#define NTP_LOOP_DELAY 30000    // ms
-
-extern TimeChangeRule BST; // British Summer Time
-extern TimeChangeRule GMT;  // Standard Time
-extern Timezone UK;
+ * From the github repository https://github.com/khoih-prog/ESP_WiFiManager
+ * take one of the examples and use it as a library.
+ * In this case ConfigOnDoubleReset_Multi
+ * I did have to force the use of SPIFFS to get it to run
+ * The example code is modified with an SVB comment
+ */
 
 /* FreeRTOS Task to manage WiFi and keep time updated by NTP */
 void etask_wifi(void * parameter);
 
 /* Static functions */
 
-/**
- * Input time in epoch format and return tm time format
- * by Renzo Mischianti <www.mischianti.org> 
- */
-static tm getDateTimeByParams(long time){
-    struct tm *newtime;
-    const time_t tim = time;
-    newtime = localtime(&tim);
-    return *newtime;
-}
-
-
-/**
- * Input tm time format and return String with format pattern
- * by Renzo Mischianti <www.mischianti.org>
- */
-static String getDateTimeStringByParams(tm *newtime, char* pattern = (char *)"%d/%m/%Y %H:%M:%S"){
-    char buffer[30];
-    strftime(buffer, 30, pattern, newtime);
-    return buffer;
-}
-
-
-/**
- * Input time in epoch format and return String with format pattern
- * by Renzo Mischianti <www.mischianti.org> 
- */
-static String getEpochStringByParams(long time, char* pattern = (char *)"%d/%m/%Y %H:%M:%S"){
-//    struct tm *newtime;
-    tm newtime;
-    newtime = getDateTimeByParams(time);
-    return getDateTimeStringByParams(&newtime, pattern);
-}
 
 #endif
